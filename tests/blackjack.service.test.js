b757857bde39c886af1399a22db95c229cde1dd9
Luiz Fernando Consalter Foncatti <luifon@alunos.utfpr.edu.br>
2024-04-08 14:41:54 -0300

    JACK-0000: blackjackservice with 100% coverage

--- tests/blackjack.service.test.js
+++ tests/blackjack.service.test.js
@@ -7,6 +7,29 @@ describe('BlackjackService', () => {
     blackjackService = new BlackjackService();
   });
 
+  test('startGame should initialize game state correctly', () => {
+    const mockDeck = [
+      { value: '2', suit: 'H' },
+      { value: '3', suit: 'D' },
+      { value: '4', suit: 'C' },
+      { value: '5', suit: 'S' }
+    ];
+    const expectedPlayerHand = mockDeck.slice(2, 4).reverse();
+    const expectedDealerHand = mockDeck.slice(0, 2).reverse();
+    blackjackService.createDeck = jest.fn(() => mockDeck);
+    blackjackService.shuffleDeck = jest.fn();
+
+    blackjackService.startGame();
+
+    expect(blackjackService.createDeck).toHaveBeenCalled();
+    expect(blackjackService.shuffleDeck).toHaveBeenCalledWith(mockDeck);
+    expect(blackjackService.deck).toEqual(mockDeck);
+    expect(blackjackService.player.hand).toEqual(expectedPlayerHand);
+    expect(blackjackService.player.score).toBe(blackjackService.calculateHandScore(expectedPlayerHand));
+    expect(blackjackService.dealer.hand).toEqual(expectedDealerHand);
+    expect(blackjackService.dealer.score).toBe(blackjackService.calculateHandScore([expectedDealerHand[0]]));
+  });
+
   test('resetRounds should reset rounds for player and dealer', () => {
     blackjackService.player.rounds = 5;
     blackjackService.dealer.rounds = 3;
@@ -45,9 +68,6 @@ describe('BlackjackService', () => {
 
     const drawnCard = blackjackService.drawCard();
 
-    console.log(blackjackService.deck);
-    console.log(blackjackService.deck.length);
-
     expect(blackjackService.deck.length).toBe(initialLength - 1);
     expect(initialDeck).not.toContainEqual(drawnCard);
   });
@@ -69,7 +89,46 @@ describe('BlackjackService', () => {
 
   test('determineWinner should correctly determine the winner', () => {
     expect(blackjackService.determineWinner(20, 18)).toBe('Player');
+    expect(blackjackService.determineWinner(20, 22)).toBe('Player');
+    expect(blackjackService.determineWinner(22, 20)).toBe('Dealer');
     expect(blackjackService.determineWinner(17, 20)).toBe('Dealer');
     expect(blackjackService.determineWinner(21, 21)).toBe('draw');
+  });
+
+  test('Should add a card to the players hand and return the new score', () => {
+    blackjackService.deck = [
+      { value: '2', suit: 'H' },
+      { value: 'A', suit: 'D' },
+    ];
+    blackjackService.dealer = {
+      hand: [],
+      score: 0,
+      rounds: 0
+    }
+
+    const initialScore = blackjackService.player.score;
+    const newScore = blackjackService.playerHit();
+
+    expect(newScore).toBeGreaterThan(initialScore);
+    expect(blackjackService.player.hand.length).toBeGreaterThan(0);
+  });
+
+  test('Should add cards to the dealers hands until dealer has 17 or more points for score', () => {
+    blackjackService.deck = [
+      { value: 'A', suit: 'H' },
+      { value: '5', suit: 'H' },
+    ];
+    blackjackService.dealer = {
+      hand: [
+        { value: '2', suit: 'H' },
+        { value: 'A', suit: 'D' },
+      ],
+      score: 13,
+      rounds: 0
+    }
+
+    blackjackService.dealerTurn();
+
+    expect(blackjackService.dealerScore).toBeGreaterThanOrEqual(17);
   });
 });
