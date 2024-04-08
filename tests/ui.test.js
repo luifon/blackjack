a17ff858de684ed8012ee985c5785a7dddc1b767
Luiz Fernando Consalter Foncatti <luifon@alunos.utfpr.edu.br>
2024-04-08 13:49:03 -0300

    JACK-0000: 100% coverage on ui.js

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -38,7 +38,9 @@ describe('UI Class', () => {
       { value: '10', suit: 'H' },
       { value: 'J', suit: 'D' },
     ];
+
     ui.displayPlayerHand(cards);
+
     expect(document.getElementById('player-hand').children.length).toBe(2);
   });
 
@@ -47,12 +49,29 @@ describe('UI Class', () => {
       { value: '10', suit: 'H' },
       { value: 'J', suit: 'D' },
     ];
+
     ui.displayDealerHand(cards);
+
     expect(document.getElementById('dealer-hand').children.length).toBe(2);
+  });
+
+  test('displayDealerHand hides the second card when hideSecondCard is true', () => {
+    const cards = [
+      { value: '2', suit: 'H' },
+      { value: '4', suit: 'D' }
+    ];
+
+    ui.displayDealerHand(cards, true);
+
+    const dealerHandElement = document.getElementById('dealer-hand');
+    const cardElements = dealerHandElement.querySelectorAll('.card');
+    expect(cardElements.length).toBe(2);
+    expect(cardElements[1].src).toContain('/cards/BACK.png');
   });
 
   test('Display player score', () => {
     ui.displayPlayerScore(21);
+
     expect(document.getElementById('player-score').textContent).toBe(
       'Player Score: 21'
     );
@@ -60,6 +79,7 @@ describe('UI Class', () => {
 
   test('Display dealer score', () => {
     ui.displayDealerScore(18);
+
     expect(document.getElementById('dealer-score').textContent).toBe(
       'Dealer Score: 18'
     );
@@ -67,17 +87,20 @@ describe('UI Class', () => {
 
   test('Display message', () => {
     ui.displayMessage('Hello');
+
     expect(document.getElementById('message').textContent).toBe('Hello');
   });
 
   test('Update rounds', () => {
     ui.updateRounds(1, 2);
+
     expect(document.getElementById('player-round-score').textContent).toBe('1');
     expect(document.getElementById('dealer-round-score').textContent).toBe('2');
   });
 
   test('Clear UI', () => {
     ui.clearUI(true);
+
     expect(document.getElementById('player-hand').innerHTML).toBe('');
     expect(document.getElementById('dealer-hand').innerHTML).toBe('');
     expect(document.getElementById('player-score').textContent).toBe('');
@@ -89,12 +112,14 @@ describe('UI Class', () => {
 
   test('Disable buttons', () => {
     ui.disableButtons();
+
     expect(document.getElementById('hit-button').disabled).toBe(true);
     expect(document.getElementById('stand-button').disabled).toBe(true);
   });
 
   test('Enable buttons', () => {
     ui.enableButtons();
+
     expect(document.getElementById('hit-button').disabled).toBe(false);
     expect(document.getElementById('stand-button').disabled).toBe(false);
   });
